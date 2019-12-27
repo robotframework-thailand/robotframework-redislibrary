eb3e8759b251b34ec4e249932396047df3389958
Apiwat Manasmascharoen <apiwat.man@ascendcorp.com>
2019-12-27 11:44:44 +0700

    Add keywords for operate with set in redis

--- RedisLibrary/RedisLibraryKeywords.py
+++ RedisLibrary/RedisLibraryKeywords.py
@@ -34,6 +34,31 @@ class RedisLibraryKeywords(object):
             raise Exception(str(ex))
         return redis_conn
 
+    @keyword('Flush All')
+    def flush_all(self, redis_conn):
+        """ Delete all keys from Redis
+
+        Arguments:
+            - redis_conn: Redis connection object
+
+        Examples:
+        | ${data}=   | Flush All |  ${redis_conn} |
+        """
+        return redis_conn.flushall()
+
+    @keyword('Delete From Redis')
+    def delete_from_redis(self, redis_conn, key):
+        """ Delete data from Redis
+
+        Arguments:
+            - redis_conn: Redis connection object
+            - key: String keyword to find.
+
+        Examples:
+        | Delete From Redis |  ${redis_conn} | BARCODE|1234567890 |
+        """
+        return redis_conn.delete(key)
+
     @keyword('Append To Redis')
     def append_to_redis(self, redis_conn, key, value):
         """ Append data to Redis. If key doesn't exist, create it with value.
@@ -49,59 +74,6 @@ class RedisLibraryKeywords(object):
 
         """
         return redis_conn.append(key, value)
-
-    @keyword('Get From Redis')
-    def get_from_redis(self, redis_conn, key):
-        """ Get cached data from Redis
-
-        Arguments:
-            - redis_conn: Redis connection object
-            - key: String keyword to find.
-
-        Examples:
-        | ${data}=   | Get From Redis |  ${redis_conn} | BARCODE|1234567890 |
-        """
-        return redis_conn.get(key)
-
-    @keyword('Get Set From Redis Set')
-    def get_set_from_redis_set(self, redis_conn, set_name):
-        """ Get cached members from Redis sets
-
-        Arguments:
-            - redis_conn: Redis connection object
-            - set_name: Set name to find.
-
-        Examples:
-        | ${data}=   | Get Set From Redis Set |  ${redis_conn} | BARCODE:12345:67890 |
-        """
-        return redis_conn.smembers(set_name)
-
-    @keyword('Get Dictionary From Redis Hash')
-    def get_dict_from_redis_hash(self, redis_conn, hash_name):
-        """ Get cached data from Redis hashes
-
-        Arguments:
-            - redis_conn: Redis connection object
-            - hash_name: Hash name.
-
-        Examples:
-        | ${data}=   | Get Dictionary From Redis Hash |  ${redis_conn} | HASHNAME |
-        """
-        return redis_conn.hgetall(hash_name)
-
-    @keyword('Get From Redis Hash')
-    def get_from_redis_hash(self, redis_conn, hash_name, key):
-        """ Get cached data from Redis hashes by key
-
-        Arguments:
-            - redis_conn: Redis connection object
-            - hash_name: Hash name.
-            - key: String keyword to find.
-
-        Examples:
-        | ${data}=   | Get From Redis Hash |  ${redis_conn} | HASHNAME | BARCODE|1234567890 |
-        """
-        return redis_conn.hget(hash_name, key)
 
     @keyword('Set To Redis')
     def set_to_redis(self, redis_conn, key, data, expire_time=3600):
@@ -119,32 +91,18 @@ class RedisLibraryKeywords(object):
         """
         return redis_conn.set(key, data, expire_time)
 
-    @keyword('Set To Redis Hash')
-    def set_to_redis_hash(self, redis_conn, hash_name, key, data):
-        """ Set data to Redis within Hash
+    @keyword('Get From Redis')
+    def get_from_redis(self, redis_conn, key):
+        """ Get cached data from Redis
 
         Arguments:
             - redis_conn: Redis connection object
-            - hash_name: String hash
             - key: String keyword to find.
-            - data: String data
 
         Examples:
-        | ${data}=   | Set To Redis Hash |  ${redis_conn} | HASHNAME | key | {"name":"Fred","age":25} |
+        | ${data}=   | Get From Redis |  ${redis_conn} | BARCODE|1234567890 |
         """
-        return redis_conn.hset(hash_name, key, data)
-
-    @keyword('Flush All')
-    def flush_all(self, redis_conn):
-        """ Delete all keys from Redis
-
-        Arguments:
-            - redis_conn: Redis connection object
-
-        Examples:
-        | ${data}=   | Flush All |  ${redis_conn} |
-        """
-        return redis_conn.flushall()
+        return redis_conn.get(key)
 
     @keyword('Expire Data From Redis')
     def expire_data_from_redis(self, redis_conn, key, expire_time=0):
@@ -186,33 +144,6 @@ class RedisLibraryKeywords(object):
         """
         return redis_conn.ttl(key)
 
-    @keyword('Delete From Redis')
-    def delete_from_redis(self, redis_conn, key):
-        """ Delete data from Redis
-
-        Arguments:
-            - redis_conn: Redis connection object
-            - key: String keyword to find.
-
-        Examples:
-        | Delete From Redis |  ${redis_conn} | BARCODE|1234567890 |
-        """
-        return redis_conn.delete(key)
-
-    @keyword('Delete From Redis Hash')
-    def delete_from_redis_hash(self, redis_conn, hash_name, key):
-        """Delete ``key`` from hash ``name``
-
-        Arguments:
-            - redis_conn: Redis connection object.
-            - hash_name: Hash keyword to find.
-            - key: String keyword to find.
-
-        Examples:
-        | Delete From Redis Hash |  ${redis_conn} | HASHNAME |  KEY |
-        """
-        return redis_conn.hdel(hash_name, key)
-
     @keyword('Redis Key Should Be Exist')
     def redis_key_should_be_exist(self, redis_conn, key):
         """ Keyword will fail if specify key doesn't exist in Redis
@@ -240,8 +171,78 @@ class RedisLibraryKeywords(object):
         | ${is_exist}= | Redis Key Should Not Be Exist | ${redis_conn} | BARCODE|1234567890 |
         """
         if redis_conn.exists(key):
-            logger.error("Key: " + key +" exist in Redis.")
+            logger.error("Key: " + key +" exists in Redis.")
             raise AssertionError
+
+    @keyword('Get Dictionary From Redis Hash')
+    def get_dict_from_redis_hash(self, redis_conn, hash_name):
+        """ Get cached data from Redis hashes
+
+        Arguments:
+            - redis_conn: Redis connection object
+            - hash_name: Hash name.
+
+        Examples:
+        | ${data}=   | Get Dictionary From Redis Hash |  ${redis_conn} | HASHNAME |
+        """
+        return redis_conn.hgetall(hash_name)
+
+    @keyword('Get From Redis Hash')
+    def get_from_redis_hash(self, redis_conn, hash_name, key):
+        """ Get cached data from Redis hashes by key
+
+        Arguments:
+            - redis_conn: Redis connection object
+            - hash_name: Hash name.
+            - key: String keyword to find.
+
+        Examples:
+        | ${data}=   | Get From Redis Hash |  ${redis_conn} | HASHNAME | BARCODE|1234567890 |
+        """
+        return redis_conn.hget(hash_name, key)
+
+    @keyword('Set To Redis Hash')
+    def set_to_redis_hash(self, redis_conn, hash_name, key, data):
+        """ Set data to Redis within Hash
+
+        Arguments:
+            - redis_conn: Redis connection object
+            - hash_name: String hash
+            - key: String keyword to find.
+            - data: String data
+
+        Examples:
+        | ${data}=   | Set To Redis Hash |  ${redis_conn} | HASHNAME | key | value |
+        """
+        return redis_conn.hset(hash_name, key, data)
+
+    @keyword('Add Hash Map To Redis')
+    def add_hash_map_to_redis(self, redis_conn, hash_name, dict_data):
+        """ Set data to Redis within Hash
+
+        Arguments:
+            - redis_conn: Redis connection object
+            - hash_name: String hash
+            - dict_data: data as dict
+
+        Examples:
+        | ${data}=   | Add Hash Map To Redis |  ${redis_conn} | HASHNAME | {"name":"Fred","age":25} |
+        """
+        return redis_conn.hmset(hash_name, dict_data)
+
+    @keyword('Delete From Redis Hash')
+    def delete_from_redis_hash(self, redis_conn, hash_name, key):
+        """Delete ``key`` from hash ``name``
+
+        Arguments:
+            - redis_conn: Redis connection object.
+            - hash_name: Hash keyword to find.
+            - key: String keyword to find.
+
+        Examples:
+        | Delete From Redis Hash |  ${redis_conn} | HASHNAME |  KEY |
+        """
+        return redis_conn.hdel(hash_name, key)
 
     @keyword('Redis Hash Key Should Be Exist')
     def redis_hash_key_should_be_exist(self, redis_conn, hash_name, key):
@@ -273,5 +274,93 @@ class RedisLibraryKeywords(object):
         """
         if redis_conn.hexists(hash_name, key):
             logger.error("Hash: " + hash_name + " and Key: " +
-                         key + " exist in Redis.")
+                         key + " exists in Redis.")
             raise AssertionError
+
+    @keyword('Get Set From Redis Set')
+    def get_set_from_redis_set(self, redis_conn, set_name):
+        """ Get cached members from Redis sets.
+
+        Arguments:
+            - redis_conn: Redis connection object
+            - set_name: Set name to find.
+
+        Examples:
+        | ${data}=   | Get Set From Redis Set |  ${redis_conn} | Fruit |
+        """
+        return redis_conn.smembers(set_name)
+
+    @keyword('Add Set Data To Redis Set')
+    def add_set_data_to_redis_set(self, redis_conn, set_name, *args):
+        """ Add set data into Redis.
+
+        Arguments:
+            - redis_conn: Redis connection object
+            - set_name: Set name as key in redis
+            - *args: Item that you need to put in set
+
+        Examples:
+        | Add Set Data To Redis Set |  ${redis_conn} | Fruit | Banana | Apple | Orage |
+        """
+        return redis_conn.sadd(set_name, *args)
+
+    @keyword('Item Should Exist In Redis Set')
+    def item_should_exist_in_redis_set(self, redis_conn, set_name, item):
+        """ Check item should exist in set.
+
+        Arguments:
+            - redis_conn: Redis connection object
+            - set_name: Set name as key in redis
+            - Item: Item that you need check
+
+        Examples:
+        | Item Should Exist In Redis Set |  ${redis_conn} | Fruit | Apple |
+        """
+        if not redis_conn.sismember(set_name, item):
+            logger.error("Item: " + item + " doesn't exist in Redis.")
+            raise AssertionError
+
+    @keyword('Item Should Not Exist In Redis Set')
+    def item_should_not_exist_in_redis_set(self, redis_conn, set_name, item):
+        """ Check item should not exist in set.
+
+        Arguments:
+            - redis_conn: Redis connection object
+            - set_name: Set name as key in redis
+            - Item: Item that you need check
+
+        Examples:
+        | Item Should Not Exist In Redis Set |  ${redis_conn} | Fruit | Mongo |
+        """
+        if redis_conn.sismember(set_name, item):
+            logger.error("Item: " + item + " exists in Redis.")
+            raise AssertionError
+
+    @keyword('Get Length of Redis Set')
+    def get_length_of_redis_set(self, redis_conn, set_name):
+        """ Get length of set.
+
+        Arguments:
+            - redis_conn: Redis connection object
+            - set_name: Set name as key in redis
+
+        Examples:
+        | ${set_length} | Get Length of Redis Set |  ${redis_conn} | Fruit |
+        """
+        return redis_conn.scard(set_name)
+
+    @keyword('Delete Set Data In Redis Set')
+    def delete_set_data_in_redis_set(self, redis_conn, set_name, *args):
+        """ Delete set of item from set.
+
+            If you need to delete set from redis use 'Delete From Redis' keyword.
+
+        Arguments:
+            - redis_conn: Redis connection object
+            - set_name: Set name as key in redis
+            - *args: Item that you need to delete from set
+
+        Examples:
+        | Delete Set Data In Redis Set |  ${redis_conn} | Fruit | Banana | Orage |
+        """
+        return redis_conn.srem(set_name, *args)
--- tests/test_RedisLibrary.py
+++ tests/test_RedisLibrary.py
@@ -17,6 +17,18 @@ class RedisLibraryTest(unittest.TestCase):
         self.fake_redis.set('name', 'nottyo', px=300000)
         self.fake_redis.set('home_address', '1111', ex=600000)
 
+    def test_flush_all(self):
+        self.redis.flush_all(self.fake_redis)
+        home_address = self.redis.get_from_redis(self.fake_redis, 'home_address')
+        name = self.redis.get_from_redis(self.fake_redis, 'name')
+        self.assertIsNone(home_address)
+        self.assertIsNone(name)
+
+    def test_delete_from_redis(self):
+        self.redis.delete_from_redis(self.fake_redis, 'home_address')
+        home_address = self.redis.get_from_redis(self.fake_redis, 'home_address')
+        self.assertIsNone(home_address)
+
     def test_append_to_redis(self):
         detail_address = {'city': 'Bangkok', 'country': 'Thailand'}
         self.redis.append_to_redis(self.fake_redis, 'detail_address', str(detail_address))
@@ -28,19 +40,19 @@ class RedisLibraryTest(unittest.TestCase):
         data = self.redis.get_from_redis(self.fake_redis, 'home_address').decode('UTF-8')
         self.assertEqual(data, '2222')
 
-    def test_get_ttl(self):
-        ttl = self.redis.get_time_to_live_in_redis(self.fake_redis, 'name')
-        self.assertEqual(ttl, 5)
-
-    def test_delete_from_redis(self):
-        self.redis.delete_from_redis(self.fake_redis, 'home_address')
-        home_address = self.redis.get_from_redis(self.fake_redis, 'home_address')
-        self.assertIsNone(home_address)
-
-    def test_expire_from_redis(self):
+    def test_expire_data_from_redis(self):
         self.redis.expire_data_from_redis(self.fake_redis, 'home_address', expire_time=0)
         home_address = self.redis.get_from_redis(self.fake_redis, 'home_address')
         self.assertIsNone(home_address)
+
+    def test_get_time_to_live_in_redis(self):
+        ttl = self.redis.get_time_to_live_in_redis(self.fake_redis, 'name')
+        self.assertEqual(ttl, 5)
+
+    def test_get_time_to_live_in_redis_second(self):
+        self.fake_redis.set('year', '2020', 3600)
+        ttl = self.redis.get_time_to_live_in_redis_second(self.fake_redis, 'name')
+        self.assertEqual(ttl, 300)
 
     def test_redis_key_should_be_exist_success(self):
         self.redis.redis_key_should_be_exist(self.fake_redis, 'name')
@@ -49,13 +61,6 @@ class RedisLibraryTest(unittest.TestCase):
         with self.assertRaises(AssertionError):
             self.redis.redis_key_should_be_exist(self.fake_redis, 'non_existing_key')
 
-    def test_flush_all(self):
-        self.redis.flush_all(self.fake_redis)
-        home_address = self.redis.get_from_redis(self.fake_redis, 'home_address')
-        name = self.redis.get_from_redis(self.fake_redis, 'name')
-        self.assertIsNone(home_address)
-        self.assertIsNone(name)
-
     def test_redis_key_should_not_be_exist_success(self):
         self.redis.redis_key_should_not_be_exist(self.fake_redis, 'non_existing_key')
 
@@ -63,25 +68,18 @@ class RedisLibraryTest(unittest.TestCase):
         with self.assertRaises(AssertionError):
             self.redis.redis_key_should_not_be_exist(self.fake_redis, 'name')
 
-    def test_get_set_from_redis_set(self):
-        self.fake_redis.sadd("fruit","banana")
-        self.fake_redis.sadd("fruit","apple")
-        fruit = self.redis.get_set_from_redis_set(self.fake_redis, 'fruit')
-        self.assertEqual(fruit, {b'banana', b'apple'})
-
-    def test_get_time_to_live_in_redis_second(self):
-        self.fake_redis.set('year', '2020', 3600)
-        ttl = self.redis.get_time_to_live_in_redis(self.fake_redis, 'name')
-        self.assertEqual(ttl, 5.0)
+    def test_get_dict_from_redis_hash(self):
+        self.redis.set_to_redis_hash(self.fake_redis, '7498d0b2', 'star_01', 'Pluto')
+        self.redis.set_to_redis_hash(self.fake_redis, '7498d0b2', 'star_02', 'Mars')
+        self.assertEqual(self.redis.get_dict_from_redis_hash(self.fake_redis, '7498d0b2'), {b'star_01': b'Pluto', b'star_02': b'Mars'})
 
     def test_set_to_redis_hash(self):
         self.redis.set_to_redis_hash(self.fake_redis, '7498d0b2', 'star_01', 'Pluto')
         self.assertEqual(self.redis.get_from_redis_hash(self.fake_redis, '7498d0b2', 'star_01'), b'Pluto')
 
-    def test_get_dict_from_redis_hash(self):
-        self.redis.set_to_redis_hash(self.fake_redis, '7498d0b2', 'star_01', 'Pluto')
-        self.redis.set_to_redis_hash(self.fake_redis, '7498d0b2', 'star_02', 'Mars')
-        self.assertEqual(self.redis.get_dict_from_redis_hash(self.fake_redis, '7498d0b2'), {b'star_01': b'Pluto', b'star_02': b'Mars'})
+    def test_add_hash_map_to_redis(self):
+        self.redis.add_hash_map_to_redis(self.fake_redis, '7498d0b1', {'star_01':'Saturn','star_02':'Jupiter'})
+        self.assertEqual(self.redis.get_dict_from_redis_hash(self.fake_redis, '7498d0b1'), {b'star_01': b'Saturn', b'star_02': b'Jupiter'})
 
     def test_delete_from_redis_hash(self):
         self.redis.set_to_redis_hash(self.fake_redis, '7498d0b2', 'star_01', 'Pluto')
@@ -111,6 +109,37 @@ class RedisLibraryTest(unittest.TestCase):
         self.redis.set_to_redis_hash(self.fake_redis, '7498d0b2', 'star_01', 'Pluto')
         with self.assertRaises(AssertionError):
             self.redis.redis_hash_key_should_not_be_exist(self.fake_redis, '7498d0b2', 'star_01')
+
+    def test_get_set_from_redis_set(self):
+        self.redis.add_set_data_to_redis_set(self.fake_redis, "fruit", "banana", "apple", "orage")
+        self.assertEqual(self.redis.get_set_from_redis_set(self.fake_redis, 'fruit'), {b'banana', b'apple', b"orage"})
+
+    def test_item_should_exist_in_redis_set_success(self):
+        self.redis.add_set_data_to_redis_set(self.fake_redis, "fruit", "banana", "apple", "orage")
+        self.redis.item_should_exist_in_redis_set(self.fake_redis, "fruit", "apple")
+
+    def test_item_should_exist_in_redis_set_failed(self):
+        self.redis.add_set_data_to_redis_set(self.fake_redis, "fruit", "banana", "apple", "orage")
+        with self.assertRaises(AssertionError):
+            self.redis.item_should_exist_in_redis_set(self.fake_redis, "fruit", "mongo")
+
+    def test_item_should_not_exist_in_redis_set_success(self):
+        self.redis.add_set_data_to_redis_set(self.fake_redis, "fruit", "banana", "apple", "orage")
+        with self.assertRaises(AssertionError):
+            self.redis.item_should_not_exist_in_redis_set(self.fake_redis, "fruit", "apple")
+
+    def test_item_should_not_exist_in_redis_set_failed(self):
+        self.redis.add_set_data_to_redis_set(self.fake_redis, "fruit", "banana", "apple", "orage")
+        self.redis.item_should_not_exist_in_redis_set(self.fake_redis, "fruit", "mongo")
+
+    def test_get_length_of_redis_set(self):
+        self.redis.add_set_data_to_redis_set(self.fake_redis, "fruit", "banana", "apple", "orage")
+        self.assertEqual(self.redis.get_length_of_redis_set(self.fake_redis, 'fruit'), 3)
+
+    def test_delete_set_data_in_redis_set(self):
+        self.redis.add_set_data_to_redis_set(self.fake_redis, "fruit", "banana", "apple", "orage", "mongo")
+        self.redis.delete_set_data_in_redis_set(self.fake_redis, "fruit", "banana", "orage")
+        self.assertEqual(self.redis.get_set_from_redis_set(self.fake_redis, 'fruit'), {b'apple', b'mongo'})
 
     def tearDown(self):
         self.fake_redis.flushall()
